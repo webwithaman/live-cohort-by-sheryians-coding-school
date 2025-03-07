e7aca996b1258e64cd34367dfa969d0b57afbd2d
aman <webwithaman06@gmail.com>
2025-03-07 17:22:57 +0530

    js/assignment-02/question-02

--- assignments/javascript/assignment-02/question-01/script.js
+++ assignments/javascript/assignment-02/question-01/script.js
@@ -1,13 +1,3 @@
-/*
-
-Question 1:
-
-let i = 11;
-i = i++ + ++i;
-console.log(i);
-
-*/
-
 let i = 11;
 i = i++ + ++i;
 console.log(i);
---
+++ assignments/javascript/assignment-02/question-02/script.js
@@ -0,0 +1,9 @@
+let a = 11,
+  b = 22,
+  c;
+
+c = a + b + a++ + b++ + ++a + ++b;
+
+console.log("a=" + a);
+console.log("b=" + b);
+console.log("c=" + c);
